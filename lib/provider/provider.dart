6eb99ae3dbc8381e6e6122d4876d15865b5f9d59
Raj Virani <6587rajvirani@gmail.com>
2022-10-06 22:55:58 -0700

    06/10/2022

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:news_api/model/api_news.dart';
+
+import '../model/ApiNews.dart';
 
 
 
 class Api_Provider extends ChangeNotifier {
 
   ApiNews? Datapick;
-  String newslike = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=870ee857647b4b2083a6d45768f26d3a";
+  String newslike = "https://newsapi.org/v2/everything?q=tesla&from=2022-09-06&sortBy=publishedAt&apiKey=870ee857647b4b2083a6d45768f26d3a";
 
   Future<ApiNews> Apifactory() async {
     var newsString = await http.get(Uri.parse(newslike));
--- lib/view/firstscreen.dart
+++ lib/view/firstscreen.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:news_api/model/api_news.dart';
 import 'package:news_api/provider/provider.dart';
 import 'package:provider/provider.dart';
+
+import '../model/ApiNews.dart';
 
 class newsApi_FirstScreen extends StatefulWidget {
   const newsApi_FirstScreen({Key? key}) : super(key: key);
@@ -13,28 +15,60 @@ class newsApi_FirstScreen extends StatefulWidget {
 class _newsApi_FirstScreenState extends State<newsApi_FirstScreen> {
   Api_Provider? apiproviderT;
   Api_Provider? apiproviderF;
+
   @override
   Widget build(BuildContext context) {
-    apiproviderF = Provider.of<Api_Provider>(context,listen: false);
-    apiproviderT = Provider.of<Api_Provider>(context,listen: true);
-    return SafeArea(child: Scaffold(
+    apiproviderF = Provider.of<Api_Provider>(context, listen: false);
+    apiproviderT = Provider.of<Api_Provider>(context, listen: true);
+    return SafeArea(
+        child: Scaffold(
       body: FutureBuilder<ApiNews>(
-        future:apiproviderF!.Apifactory(),
-        builder: (context,snapshot){
-          if(snapshot.hasError)
-            {
-              return Text("${snapshot.error}");
-            }
-          else if(snapshot.hasData)
-            {
-              ApiNews apimodel = snapshot.data!;
-              return ListView.builder(itemCount: apimodel.articles!.length,
-                  itemBuilder: (context,index){
-                return ListTile();
-                  }
-                  );
+        future: apiproviderF!.Apifactory(),
+        builder: (context, snapshot)
+        {
+          if (snapshot.hasError)
+          {
+            return Text("${snapshot.error}");
+          } else if (snapshot.hasData)
+          {
+            ApiNews apimodel = snapshot.data!;
+            return ListView.builder(
+                itemCount: apimodel.articles!.length,
+                itemBuilder: (context, index)
+                {
+                  return Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Container(padding: EdgeInsets.symmetric(horizontal: 8),height: 120,width: MediaQuery.of(context).size.width*0.45,child: Text("${apimodel.articles![index].title}",style: TextStyle(fontWeight:FontWeight.bold),)),
+                              Container(padding: EdgeInsets.symmetric(horizontal: 8),height: 85,width: MediaQuery.of(context).size.width*0.45,child: Text("${apimodel.articles![index].description}")),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: CachedNetworkImage(
 
-            }
+                              height: 180,
+                              width: MediaQuery.of(context).size.width*0.45,
+                              fit: BoxFit.cover,
+                              imageUrl: "${apimodel.articles![index].urlToImage}",
+                              placeholder: (context,_)=>Image.asset("assets/image/pexels-cottonbro-3944454.jpg"),
+                              errorWidget: (context,_,__)=>Image.asset("assets/image/pexels-cottonbro-3944454.jpg"),
+                            ),
+                          ),
+
+
+                        ],
+                      ),
+                      SizedBox(height: 40,)
+                    ],
+                  );
+                }
+              );
+          }
           return Center(child: CircularProgressIndicator());
         },
       ),
